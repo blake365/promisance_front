51458b872926e1000508f2cb970313b3dd1895d5
blake365 <68243611+blake365@users.noreply.github.com>
2023-09-17 14:38:33 -0400

    link fix

--- src/index.jsx
+++ src/index.jsx
@@ -51,7 +51,7 @@ inject();
 // import Guide from './components/guide/guide'
 
 if (import.meta.env.PROD) {
-	Axios.defaults.baseURL = 'https://api.neopromisance.com'
+	Axios.defaults.baseURL = 'https://api.neopromisance.com/api'
 } else {
 	Axios.defaults.baseURL = 'http://localhost:5001/api'
 }
