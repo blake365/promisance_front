f614f01067eef9cdf1a36cdcbf272602a1f4d10c
blake365 <68243611+blake365@users.noreply.github.com>
2024-02-01 17:31:38 -0500

    Update round start and end dates, and turns frequency

--- api/cron/turns.js
+++ api/cron/turns.js
@@ -1,11 +1,11 @@
-import { ROUND_START, ROUND_END } from '../../src/config/config'
+import { ROUND_START, ROUND_END, TURNS_FREQ } from '../../src/config/config'
 export const edge = true
 
 export default async function handler(request, response) {
 	let now = new Date().getTime()
 	if (
 		now > new Date(ROUND_START).getTime() &&
-		now < new Date(ROUND_END).getTime()
+		now < new Date(ROUND_END).getTime() + 1000 * 60 * TURNS_FREQ + 1000
 	) {
 		const result = await fetch('https://api.neopromisance.com/api/cron/turns', {
 			headers: {
