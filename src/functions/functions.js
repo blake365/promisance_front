d4a77ed62d55427058a2426075da61be4a1ff057
blake365 <68243611+blake365@users.noreply.github.com>
2024-04-01 18:02:32 -0400

    rot update

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -157,8 +157,10 @@ export function calcProvisions(empire) {
 export function calcRot(empire, consumption) {
 	let rot = 0
 	let percentFarms = empire.bldFood / empire.land
-	if (percentFarms < 0.01) percentFarms = 0.01
-	if (empire.food > empire.networth * 35 * percentFarms) {
+	if (percentFarms < 0.1) percentFarms = 0.1
+	if (empire.networth < 10000000) {
+		rot = 0
+	} else if (empire.food > empire.networth * 35 * percentFarms) {
 		const multiples = Math.floor(empire.food / empire.networth) - 1
 		rot = Math.round(
 			(multiples / 2) * empire.food * 0.001 * 0.01 * (1 - percentFarms)
